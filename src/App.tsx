a44d38b425711fada3c7bf4adb68530331ea8714
AmanCH3 <armanachaudhary53@gmail.com>
2024-05-27 11:37:11 +0545

    feat : Assignment task carrom board design

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {useForm} from 'react-hook-form' ;
 
 function App() {
-  const [count, setCount] = useState(0)
+    const {register ,handleSubmit,formState:{errors}} = useForm({
+        initialValues:{name:""},
+        defaultValues : {name:""}
+
+    });
+    console.log(errors)
+
+    const onSubmit = (data) =>{
+        console.log(data);
+    }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+            <label> name </label>
+            <input type = "text"{...register("name",{required:"This is required Field"})} />
+            <span className= "text-danger">{errors?.name?.message}</span>
+        </div>
+
+        <div>
+            <input type = "submit"/>
+        </div>
+        </form>
     </>
   )
 }
